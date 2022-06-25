90375faa5070707a202076052261c530c8cc4726
Ahmad Tiar K <97088690+ATiarK@users.noreply.github.com>
2022-06-25 22:04:50 +0700

    configure axios

---
+++ src/api/api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const API = "https://api.themoviedb.org/3";
+
+export const getGenres = async (setData) => {
+  await axios
+    .get(`${API}/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}`)
+    .then((res) => {
+      // console.log(res.data.genres);
+      setData(res.data.genres);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+};
+
+export const getList = async (setData, page) => {
+  await axios
+    .get(
+      `${API}/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
+    )
+    .then((res) => {
+      // console.log(res.data.results);
+      setData(res.data.results);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+};
+
+export const getDetails = async (setData, setGenres, setCompanies, id) => {
+  await axios
+    .get(`${API}/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}`)
+    .then((res) => {
+      // console.log(res.data);
+      setData(res.data);
+      setGenres(res.data.genres);
+      setCompanies(res.data.production_companies);
+      document.title = res.data.title;
+    })
+    .catch((err) => {
+      alert(err);
+    });
+};
